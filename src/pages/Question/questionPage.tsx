9bd1af88a3c8126eb32fe8fd2d3d9160024d4d8d
user9381 <rudyboullier.info@gmail.com>
2023-12-08 05:50:28 +0100

    feat: add score

--- src/pages/Question/questionPage.tsx
+++ src/pages/Question/questionPage.tsx
@@ -1,34 +1,43 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./question.css";
 import data from "../../data/data.json";
-import {Question} from "../../types/question";
+import { Question } from "../../types/question";
 
 const QuestionPage: React.FC = () => {
     const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
     const [showExplanation, setShowExplanation] = useState(false);
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [score, setScore] = useState(0);
+    const [isButtonLocked, setIsButtonLocked] = useState(false);
 
     const questions = data.questions;
     const myQuestion: Question = questions[currentQuestionIndex];
+    const assetPath = "../../assets/";
 
-    const ASSETPATH = "../../assets/"
+    const handleScore = () => setScore(score + 1);
 
-    const handleAnswerClick = (index: number) => {
+    const handleAnswerClick = (index: number, truth: boolean) => {
         setSelectedAnswer(index);
         setShowExplanation(true);
+
+        if (truth) {
+            handleScore();
+        }
+        setIsButtonLocked(true);
     };
 
-    const renderAnswers = () => {
-        return myQuestion.answers.map((answer, index) => (
-            <div key={'answer.${index}'} className="answer">
+    const renderAnswers = () =>
+        myQuestion.answers.map((answer, index) => (
+            <div key={`answer.${index}`} className="answer">
                 <button
                     tabIndex={0}
-                    onClick={() => handleAnswerClick(index)}
+                    onClick={() => handleAnswerClick(index, answer.truth)}
                     onKeyDown={(e) => {
-                        if (e.key === 'Enter' || e.key === ' ') {
-                            handleAnswerClick(index);
+                        if (e.key === "Enter" || e.key === " ") {
+                            handleAnswerClick(index, answer.truth);
                         }
                     }}
+                    disabled={isButtonLocked}
                 >
                     <div className={selectedAnswer === index ? "selected" : ""}>
                         {answer.title}
@@ -36,50 +45,56 @@ const QuestionPage: React.FC = () => {
                 </button>
             </div>
         ));
-    };
 
-    const renderExplanation = () => {
-        return (
-            <div>
-                {myQuestion.data.explanations.map((explanation) => (
-                    <div key={'explanation.${index}'}>
-                        {explanation.type === "paragraph" && (
-                            <p>{explanation.value}</p>
-                        )}
-                        {explanation.type === "button" && (
-                            <a href={explanation.link}>{explanation.value}</a>
-                        )}
-                    </div>
-                ))}
-                <div>
-                    <img src={`${ASSETPATH}/answers/${myQuestion.data.image.link}`}
-                         alt={myQuestion.data.image.description} loading="lazy"/>
+    const renderExplanation = () => (
+        <div>
+            {myQuestion.data.explanations.map((explanation, index) => (
+                <div key={`explanation.${index}`}>
+                    {explanation.type === "paragraph" && (
+                        <p>{explanation.value}</p>
+                    )}
+                    {explanation.type === "button" && (
+                        <a href={explanation.link}>{explanation.value}</a>
+                    )}
                 </div>
+            ))}
+            <div>
+                <img
+                    src={`${assetPath}/answers/${myQuestion.data.image.link}`}
+                    alt={myQuestion.data.image.description}
+                    loading="lazy"
+                />
             </div>
-        );
-    };
+        </div>
+    );
 
     const resetState = () => {
         setSelectedAnswer(null);
         setShowExplanation(false);
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
+        setCurrentQuestionIndex((prevIndex) =>
+            prevIndex < questions.length - 1 ? prevIndex + 1 : prevIndex
+        );
+        setIsButtonLocked(false);
+    };
+
+    const handleNextQuestionClick = () => {
+        resetState();
     };
 
     return (
-        <div className="mainContainer">
-            <div id="question">
-                <div id="title">
-                    {myQuestion.title}
+        <div className="question-containers">
+            <h1>Score: {score}</h1>
+            <div className="mainContainer">
+                <div id="question">
+                    <div id="title">{myQuestion.title}</div>
+                    <div id="answers">{renderAnswers()}</div>
+                    {showExplanation && renderExplanation()}
+                    {showExplanation && (
+                        <button onClick={handleNextQuestionClick}>
+                            Next Question
+                        </button>
+                    )}
                 </div>
-                <div id="answers">
-                    {renderAnswers()}
-                </div>
-                {showExplanation && renderExplanation()}
-                {showExplanation && (
-                    <button onClick={resetState}>
-                        Next Question
-                    </button>
-                )}
             </div>
         </div>
     );
