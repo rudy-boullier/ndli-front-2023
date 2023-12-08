2d9c2520786a3c19ead4c0c0b764e2d0c316095c
Vincent C <vincent.chavot----dambrun@etu.univ-lyon1.fr>
2023-12-08 04:22:30 +0100

    feat: add home page information content

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Home.css";
 
+import HomeMainContainer from "./HomeMainContainer.tsx";
+
 const Home: React.FC = () => {
     return (
         <div id="home">
             <section className="hero-banner">
-                <h1>Home page</h1>
+                <HomeMainContainer />
             </section>
         </div>
     );
